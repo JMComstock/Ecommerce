444a5464226c6b3550451f4ea3648c24b1490f87
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-03-04 23:18:04 -0500

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yujr0he*k#1%rnd%)3@o@0bw8k&=kb683%9w=!)79i+24ab^^^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
